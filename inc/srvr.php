c3442e0f0e13d92c6f5ea5f92fa5e608af01c511
Ludwig Bethnicer Cagas-Napigkit <napigkitludwigbethnicer@gmail.com>
2022-03-31 07:32:38 +0800

    google auth

--- inc/srvr.php
+++ inc/srvr.php
@@ -9,18 +9,4 @@
 	$dir		= '/alphaphpn.com/staging/termarshardwareandconstruction/'; // Domain Directory
 	$lnkfont	= '//fonts.googleapis.com/css?family=Sofia'; // Google Font
 
-	// $company	= "Company"; // Company Name
-	// $title		= "Title"; // System Title
-	// $tagline	= "Quote, saying or moto"; // Tagline
-	// $owner		= "Owner T. Businness";
-	// $email		= "info@contact.com"; // E-mail
-	// $telno		= "+323332469"; // Telephone Number
-	// $mobileno	= "+639094826025"; // Mobile Number
-	// $address	= "Sed ut perspiciatis unde omnis iste natus"; // Address
-	// $gps		= "7.784875,122.592581"; // Google Map Location
-	// $build_by	= "Who";
-	// $cwebzite	= "google.com";
-	// $dcurrencyx	= "$";
-
-	// $gauthlogin	= "915105451708-09slv78bomdcnij292kvgmfijv229f3i.apps.googleusercontent.com";
-	$gauthlogin	= '775302666686-j83mcm9edhjb2gd80a7k3ctehpuuqipv.apps.googleusercontent.com';
+	$gauthlogin	= '765265328491-t335crpsfmvu4t4r3a5o0u2h3ol45a57.apps.googleusercontent.com';
